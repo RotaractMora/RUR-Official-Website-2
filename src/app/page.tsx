b388feac3b880a2323362acd526f7fe7da98521f
Sakindu Ransindu <sakindu.ransindu@gmail.com>
2024-11-12 20:23:41 +0530

    add&delete files functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,9 @@ import { addSponsor, deleteSponsor, getSponsors } from "@/services/sponsors.serv
 import { addTimeLineEvent, deleteTimeLineEvent, getTimeLineEvents } from "@/services/timeline.service";
 import { ITimelineData } from "@/interfaces/ITimeline";
 import { title } from "process";
-import { addImageFile } from "@/services/firebaseStorage.service";
+import { addFile, deleteFile } from "@/services/firebaseStorage.service";
+import { getDownloadURL, getStorage, ref } from "firebase/storage";
+import { Timestamp } from "firebase/firestore";
 
 
 export const products = [
@@ -183,45 +185,6 @@ const navItms = [
   },
 ];
 
-// const timeline: ITimelineEntry[] = [
-//   {
-//     title: "Heads up",
-//     content: "The curriculum vitae and job interviews play significant roles in a job recruitment process. Presenting a CV that showcases one's life work accurately is the key to open doors for a face-to-face interview.",
-//     eventDate: new Date("2024-11-11"),
-//     btnLink: "#",
-//   },
-//   {
-//     title: "The Image",
-//     content: "Personal branding is telling an individual's story in a means that reflects his conduct, behavior, attitudes,and unspoken words. A unique personal brand is what would make a professional stand out from the rest and risealong his career ladder.",
-//     eventDate: new Date("2024-11-12"),
-//     btnLink: "#",
-//   },
-//   {
-//     title: "Resume Center",
-//     content: "The curriculum vitae or the CV is the primary opportunity for a job applicant to establish a strong initial impression on a potential employer. A quality CV will surely boost the likelihood of an employment seeker getting a face-to-face interview.",
-//     eventDate: new Date("2024-11-13"),
-//     btnLink: "#",
-//   },
-//   {
-//     title:"Career Insights",
-//     content:"Most of the undergraduates manifest a lack of knowledge regarding the career paths they could pursue following their graduation. Hence, a series of webinars will be conducted based on the different career fields catered to different departments of the university by the industry and corporate companies who will be participating the careerfair.",
-//     eventDate: new Date("2024-11-14"),
-//     btnLink: "#",
-//   },
-//   {
-//     title: "Flagship Fair",
-//     content: "'Flagship Fair' is the limelight of 'Are you Ready?', in which the prospective employers' role comes intoplay. The employers are given the opportunity to identify skillful and qualified undergraduates of University of Moratuwa, through mock and real interviews.",
-//     eventDate: new Date("2024-11-15"),
-//     btnLink: "#",
-//   },
-//   {
-//     title: "Global Pathway",
-//     content: "'Global Pathway' program offers a variety of resources to help students prepare for their international job search. This includes information on how to create a global resume, how to research international job opportunities, and how to effectively network with professionals in different countries. Additionally, the program provides students with access to a wide range of job search tools, including job boards and career fairs, as well as career counseling services to help students identify and pursue their ideal global career.",
-//     eventDate: new Date("2024-11-16"),
-//     btnLink: "#",
-//   }
-// ];
-
 
 
 const Para = ({level}:{level:string}):React.ReactNode => {
@@ -250,6 +213,79 @@ export default function Home() {
         setSponsors(data);
       }
     );
+
+
+    // const timelineEvents: ITimelineData[] = [
+    //   {
+    //     title: "Heads up",
+    //     description: "The curriculum vitae and job interviews play significant roles in a job recruitment process. Presenting a CV that showcases one's life work accurately is the key to open doors for a face-to-face interview.",
+    //     eventDate: new Date("2024-11-11"),
+    //     btnLink: "#",
+    //     imgURL: "path/to/image1.jpg",
+    //     btnText: "Learn More",
+    //     order: 1,
+    //     isBtnDisabled: false,
+    //   },
+    //   {
+    //     title: "The Image",
+    //     description: "Personal branding is telling an individual's story in a means that reflects his conduct, behavior, attitudes,and unspoken words. A unique personal brand is what would make a professional stand out from the rest and risealong his career ladder.",
+    //     eventDate: new Date("2024-11-12"),
+    //     btnLink: "#",
+    //     imgURL: "path/to/image2.jpg",
+    //     btnText: "Learn More",
+    //     order: 2,
+    //     isBtnDisabled: false,
+    //   },
+    //   {
+    //     title: "Resume Center",
+    //     description: "The curriculum vitae or the CV is the primary opportunity for a job applicant to establish a strong initial impression on a potential employer. A quality CV will surely boost the likelihood of an employment seeker getting a face-to-face interview.",
+    //     eventDate: new Date("2024-11-13"),
+    //     btnLink: "#",
+    //     imgURL: "path/to/image3.jpg",
+    //     btnText: "Learn More",
+    //     order: 3,
+    //     isBtnDisabled: false,
+    //   },
+    //   {
+    //     title: "Career Insights",
+    //     description: "Most of the undergraduates manifest a lack of knowledge regarding the career paths they could pursue following their graduation. Hence, a series of webinars will be conducted based on the different career fields catered to different departments of the university by the industry and corporate companies who will be participating the careerfair.",
+    //     eventDate: new Date("2024-11-14"),
+    //     btnLink: "#",
+    //     imgURL: "path/to/image4.jpg",
+    //     btnText: "Learn More",
+    //     order: 4,
+    //     isBtnDisabled: false,
+    //   },
+    //   {
+    //     title: "Flagship Fair",
+    //     description: "'Flagship Fair' is the limelight of 'Are you Ready?', in which the prospective employers' role comes intoplay. The employers are given the opportunity to identify skillful and qualified undergraduates of University of Moratuwa, through mock and real interviews.",
+    //     eventDate: new Date("2024-11-15"),
+    //     btnLink: "#",
+    //     imgURL: "path/to/image5.jpg",
+    //     btnText: "Learn More",
+    //     order: 5,
+    //     isBtnDisabled: false,
+    //   },
+    //   {
+    //     title: "Global Pathway",
+    //     description: "'Global Pathway' program offers a variety of resources to help students prepare for their international job search. This includes information on how to create a global resume, how to research international job opportunities, and how to effectively network with professionals in different countries. Additionally, the program provides students with access to a wide range of job search tools, including job boards and career fairs, as well as career counseling services to help students identify and pursue their ideal global career.",
+    //     eventDate: new Date("2024-11-16"),
+    //     btnLink: "#",
+    //     imgURL: "path/to/image6.jpg",
+    //     btnText: "Learn More",
+    //     order: 6,
+    //     isBtnDisabled: false,
+    //   }
+    // ];
+    
+    // timelineEvents.forEach(event => {
+    //   addTimeLineEvent(event).then(response => {
+    //     console.log(`Event ${event.title} added successfully`, response);
+    //   }).catch(error => {
+    //     console.error(`Error adding event ${event.title}`, error);
+    //   });
+    // });
+    
     
     // console.log("Sponsors added",getSponsers("Silver"));
     // const s = {
@@ -262,7 +298,7 @@ export default function Home() {
     // const timelineEvent: ITimelineData = {
     //   title: "Global Pathway",
     //   description: "'Global Pathway' program offers a variety of resources to help students prepare for their international job search. This includes information on how to create a global resume, how to research international job opportunities, and how to effectively network with professionals in different countries. Additionally, the program provides students with access to a wide range of job search tools, including job boards and career fairs, as well as career counseling services to help students identify and pursue their ideal global career.",
-    //   eventDate: new Date(),
+    //   eventDate: new Date("2024-11-16 1:55:23") ,
     //   btnLink: "#",
     //   imgURL: "path/to/image.jpg",
     //   btnText: "Learn More",
@@ -273,6 +309,12 @@ export default function Home() {
     // console.log(addTimeLineEvent(timelineEvent));
     // console.log(getTimeLineEvents());
     // console.log(deleteTimeLineEvent("yGwprTd3MN2nVi0MdAoX"));
+
+    // deleteFile(ref(getStorage(),'images/test.png')).then((res)=>{
+    //   console.log("Deleted",res);
+    // }).catch((error)=>{
+    //   console.log("Error",error);
+    // });
 
 
   },[]);
@@ -290,23 +332,32 @@ export default function Home() {
           <FloatingNav navItems={navItms}/>
           <HeroParallax products={products}/>
           <Timeline data={events} />
-          <input type="file" onChange={(e)=>{
+
+          <label htmlFor="test">File</label>
+          <input id="test" name="test" type="file" onChange={(e)=>{
+            let reference;
             if(e.target.files){
-              console.log(addImageFile(e.target.files[0]));
+              reference = addFile(e.target.files[0]).then((ref)=>{
+              return ref;
+              });
+              if(reference != null){
+                 reference.then((ref)=>{
+                  if(ref){
+                     getDownloadURL(ref).then((url)=>{
+                    });
+                  }
+                })
+                .catch((error)=>{
+                  console.log("Error",error);
+                });
+              }
             }
-          }
-          }/>
+          }} />
 
           <LampLighting firstLine="Sponsers" secondLine="__________"/>
 
           <TracingBeam className="">
-            {/* <GlareCard className="w-5xl" CardColor="Gold"><Para level="Gold Sponser" /></GlareCard>
-            <GlareCard className="w-5xl" CardColor="Gold"><Para level="Gold Sponser" /></GlareCard>
-            <GlareCard className="w-5xl" CardColor="Silver"><Para level="Silver Sponser" /></GlareCard>
-            <GlareCard className="w-5xl" CardColor="Silver"><Para level="Silver Sponser" /></GlareCard>
-            <GlareCard className="w-5xl" CardColor="Bronze"><Para level="Bronze Sponser" /></GlareCard>
-            <GlareCard className="w-5xl" CardColor="Bronze"><Para level="Bronze Sponser" /></GlareCard> */}
-
+          
           {
             sponsors.filter((sponsor)=>sponsor.level === "Gold").map((sponsor, index) =>(
             <GlareCard key={`${sponsor.level}-${index}`} className="w-5xl" CardColor={sponsor.level}>
--- src/services/firebaseStorage.service.ts
+++ src/services/firebaseStorage.service.ts
@@ -1,13 +1,33 @@
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getStorage, ref, uploadBytes,getDownloadURL, StorageReference ,deleteObject} from "firebase/storage";
 import { getApp } from "firebase/app";
 
 const firebaseApp = getApp();
 const storage = getStorage(firebaseApp);
 
 
-export const addImageFile = async (file: Blob | Uint8Array | ArrayBuffer)=>{
-    const imageRefference = ref(storage, 'images');
-    await uploadBytes(imageRefference, file).then((snapshot) => {
-        console.log('file Uploaded.',snapshot);
+export const addFile = async (file: Blob | Uint8Array | ArrayBuffer):Promise<StorageReference|null>=>{
+    const timestamp = new Date().getTime();
+    const fileName = `image_${timestamp}.png`;
+    const fileRefference = ref(storage, `images/${fileName}`);
+    const reference = await uploadBytes(fileRefference, file).then((snapshot) => {
+        console.log('file Uploaded.');
+        return snapshot.ref;
+      })
+      .catch((error) => {
+        console.error('Error uploading file: ', error);
+        return null;
       });
+    return reference;
+}
+
+
+export const deleteFile = async (fileRef:StorageReference):Promise<boolean>=>{
+    await deleteObject(fileRef).then(() => {
+        console.log('File deleted');
+        return true;
+      }).catch((error) => {
+        console.error('Error deleting file: ', error);
+        return false;
+      });
+    return false;
 }
