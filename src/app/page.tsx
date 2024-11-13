a50224436f82a22edaf9bb7f73a2e24aec9d0ec3
Sakindu Ransindu <sakindu.ransindu@gmail.com>
2024-11-14 00:07:43 +0530

    fix ui and env issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,12 +19,10 @@ import RUR_IMG12 from "../../public/Images/RUR (12).jpg"
 import RUR_IMG13 from "../../public/Images/RUR (13).jpg"
 import RUR_IMG14 from "../../public/Images/RUR (14).jpg"
 import RUR_IMG15 from "../../public/Images/RUR (15).jpg"
-import RUR_IMG16 from "../../public/Images/RUR (16).jpg"
-import RUR_IMG17 from "../../public/Images/RUR (17).jpg"
-import RUR_IMG18 from "../../public/Images/RUR (18).jpg"
-import RUR_IMG19 from "../../public/Images/RUR (19).jpg"
-import RUR_IMG20 from "../../public/Images/RUR (20).jpg"
-import RUR_IMG21 from "../../public/Images/RUR (21).jpg"
+import SPONSOR1 from "../../public/Images/partners/Dimo-logo-1.svg"
+import SPONSOR2 from "../../public/Images/partners/JKH Logo.png"
+import SPONSOR3 from "../../public/Images/partners/Logo_of_MAS_Holdings.png"
+import SPONSOR4 from "../../public/Images/partners/lseg.png"
 import { Timeline ,ITimelineEntry } from "@/components/ui/timeline";
 
 import LampLighting from "@/components/ui/lamp";
@@ -42,6 +40,8 @@ import { title } from "process";
 import { addFile, deleteFile } from "@/services/firebaseStorage.service";
 import { getDownloadURL, getStorage, ref } from "firebase/storage";
 import { Timestamp } from "firebase/firestore";
+import { useLoadState , LoadedProvider } from "@/context/loadStateContext";
+import { loadProjectInfo } from "next/dist/build/webpack-config";
 
 
 export const products = [
@@ -135,44 +135,12 @@ export const products = [
     thumbnail:
       RUR_IMG15,
   },
-  {
-    title: "Are You Ready?",
-    link: "#",
-    thumbnail:
-      RUR_IMG16,
-  },
-  {
-    title: "Are You Ready?",
-    link: "#",
-    thumbnail:
-      RUR_IMG17,
-  },
-  {
-    title: "Are You Ready?",
-    link: "#",
-    thumbnail:
-      RUR_IMG18,
-  },
-  {
-    title: "Are You Ready?",
-    link: "#",
-    thumbnail:
-      RUR_IMG19,
-  },
-  {
-    title: "Are You Ready?",
-    link: "#",
-    thumbnail:
-      RUR_IMG20,
-  },
-  {
-    title: "Are You Ready?",
-    link: "#",
-    thumbnail:
-      RUR_IMG21,
-  }
   
 ];
+
+
+
+
 
 const navItms = [
   {
@@ -186,31 +154,123 @@ const navItms = [
 ];
 
 
+const grid = [
+  {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "123-456-7890",
+    position: "Software Engineer",
+  },
+  {
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    phone: "234-567-8901",
+    position: "Project Manager",
+  },
+  {
+    name: "Alice Johnson",
+    email: "alice.johnson@example.com",
+    phone: "345-678-9012",
+    position: "QA Engineer",
+  },
+  {
+    name: "Bob Brown",
+    email: "bob.brown@example.com",
+    phone: "456-789-0123",
+    position: "Software Engineer",
+  },
+  {
+    name: "Charlie Davis",
+    email: "charlie.davis@example.com",
+    phone: "567-890-1234",
+    position: "Project Manager",
+  },
+  {
+    name: "Diana Evans",
+    email: "diana.evans@example.com",
+    phone: "678-901-2345",
+    position: "QA Engineer",
+  },
+  {
+    name: "Frank Green",
+    email: "frank.green@example.com",
+    phone: "789-012-3456",
+    position: "Software Engineer",
+  },
+  {
+    name: "Grace Harris",
+    email: "grace.harris@example.com",
+    phone: "890-123-4567",
+    position: "Project Manager",
+  },
+  {
+    name: "Henry Lee",
+    email: "henry.lee@example.com",
+    phone: "901-234-5678",
+    position: "QA Engineer",
+  },
+];
 
-const Para = ({level}:{level:string}):React.ReactNode => {
+const Loading =()=> {
   return (
-    <div className="max-w-5xl mx-auto py-2 px-0 md:px-0 lg:px-1">
-      <Image src={RUR_IMG1} alt="RUR" className="p-0 rounded-lg dark:bg-black bg-white"/>
-      <h6 className="text-6xl text-center font-bold dark:text-custom-color-800 text-custom-dark-color-800 p-3">
-        {level}
-      </h6>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid border-transparent"></div>
     </div>
   );
 }
+
+
+const Para = ({level,name,imgURL,loadCallback}:{level:string,name:string,imgURL:string|undefined,loadCallback?:(count: number) => void}):React.ReactNode => {
+  const { markAsLoaded} = useLoadState();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    if(isLoaded){
+      markAsLoaded(1)
+    }
+  }
+  ,[isLoaded]);
+
+  return (
+    <div className="max-w-5xl mx-auto px-0 md:px-0 lg:px-1 flex flex-col items-center h-full justify-between">
+      <h6 className="text-6xl text-center font-bold dark:text-custom-color-800 text-custom-dark-color-800 p-2 py-3">
+        {`${level} Sponsor`}
+      </h6>
+      <Image
+        onLoad={() => setIsLoaded(true)}
+        src={imgURL ? imgURL : SPONSOR1}
+        width={300}
+        height={100}
+        alt="Sponsor"
+        className="p-0 rounded-lg dark:bg-black bg-white h-50"
+      />
+      <h5 className="text-5xl text-center font-bold dark:text-custom-color-900 text-custom-dark-color-900 p-3">
+        {name}
+      </h5>
+    </div>
+  );
+  
+}
+
 
 export default function Home() {
 
   const [timeline,setTimeline] = useState([] as ITimelineData[]);
   const [sponsors, setSponsors] = useState([] as ISponsor[]);
+  const [isTimelineLoading, setTimelineLoading] = useState(true);
+  const [isSponsorsLoading, setSponsorsLoading] = useState(true);
+  const {addToTotalImagesCount,loadProgress,totalItms,loadedItms} = useLoadState();
 
   useEffect(() => {
       
     getTimeLineEvents().then((data) => {
         setTimeline(data);
+        setTimelineLoading(false);
       });
 
     getSponsors("All").then((data) => {
         setSponsors(data);
+        setSponsorsLoading(false);
+        addToTotalImagesCount(data.filter((sponsor)=>sponsor.level === "Gold" || sponsor.level ==="Silver" || sponsor.level === "Bronze").length);
       }
     );
   },[]);
@@ -221,50 +281,62 @@ export default function Home() {
     eventDate: t.eventDate.toDate(),
     btnLink: t.btnLink,
   }));
-  console.log("Events",events);
+
+  
+
   return (
     
         <RootLayout>
           <FloatingNav navItems={navItms}/>
           <HeroParallax products={products}/>
-          <Timeline data={events} />
-
-          <LampLighting firstLine="Sponsers" secondLine="__________"/>
-
-          <TracingBeam className="">
+          {
+            isTimelineLoading ? <Loading/> : (events.length > 0 ? <Timeline data={events} /> : <div>Timeline will be updated soon.</div>)
+          }
           
-          {
-            sponsors.filter((sponsor)=>sponsor.level === "Gold").map((sponsor, index) =>(
-            <GlareCard key={`${sponsor.level}-${index}`} className="w-5xl" CardColor={sponsor.level}>
-              <Para level={` ${sponsor.name} - ${sponsor.level} Sponsor`} />
-            </GlareCard>
-            ))
-          }
-          {
-            sponsors.filter((sponsor)=>sponsor.level === "Silver").map((sponsor, index) =>(
-              <GlareCard key={`${sponsor.level}-${index}`} className="w-5xl" CardColor={sponsor.level}>
-                <Para level={` ${sponsor.name} - ${sponsor.level} Sponsor`} />
-              </GlareCard>
-            ))
-          }
-          {
-            sponsors.filter((sponsor)=>(sponsor.level == "Bronze") ).map((sponsor, index) =>(
-              <GlareCard key={`${sponsor.level}-${index}`} className="w-5xl" CardColor={sponsor.level}>
-                <Para level={` ${sponsor.name} - ${sponsor.level} Sponsor`} />
-              </GlareCard>
-            ))
-          }
 
-          </TracingBeam>
+          <LampLighting firstLine="Sponsers" secondLine=""/>
+          <button type="button" onClick={()=>console.log(loadProgress,totalItms,loadedItms)}>click {loadProgress}</button>
+
+          {
+            isSponsorsLoading ? <Loading/> : ( sponsors.length > 0 ?
+        
+                <TracingBeam className="">
+                    
+                    {
+                      sponsors.filter((sponsor)=>sponsor.level === "Gold").map((sponsor, index) =>(
+                      <GlareCard key={`${sponsor.level}-${index}`} className="w-5xl" CardColor={sponsor.level}>
+                        <Para name={sponsor.name} imgURL={sponsor.imgURL} level={sponsor.level} />
+                      </GlareCard>
+                      ))
+                    }
+                    {
+                      sponsors.filter((sponsor)=>sponsor.level === "Silver").map((sponsor, index) =>(
+                        <GlareCard key={`${sponsor.level}-${index}`} className="w-5xl" CardColor={sponsor.level}>
+                          <Para name={sponsor.name} imgURL={sponsor.imgURL} level={sponsor.level} />
+                        </GlareCard>
+                      ))
+                    }
+                    {
+                      sponsors.filter((sponsor)=>(sponsor.level == "Bronze") ).map((sponsor, index) =>(
+                        <GlareCard key={`${sponsor.level}-${index}`} className="w-5xl" CardColor={sponsor.level}>
+                          <Para name={sponsor.name} imgURL={sponsor.imgURL} level={sponsor.level}  />
+                        </GlareCard>
+                      ))
+                    }
+
+                </TracingBeam>
+                :
+                <div> Sponsors will be updated soon.</div>
+            )
+        }
 
 
           <GridBackground title="Reach Us">
-          <ReachUsSection/>
+          <ReachUsSection grid={grid} />
           </GridBackground>
 
 
           <Footer />
-
         </RootLayout>
   );
 }
