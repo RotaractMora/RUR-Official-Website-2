220908b0cf5095a751861beefb004fe0a1ec9424
Sakindu Ransindu <sakindu.ransindu@gmail.com>
2025-03-01 00:42:37 +0530

    timeline fix

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -54,7 +54,6 @@ export const Timeline = ({ data }: { data: ITimelineEntry[] }) => {
       ref={containerRef}
     >
       <div className="max-w-7xl mx-auto py-10 px-4 md:px-8 lg:px-10">
-      <div className="max-w-7xl mx-auto pt-20 px-4 md:px-8 lg:px-10">
         <h2 className="text-lg md:text-4xl mb-4 text-custom-dark-color-950 dark:text-custom-color-950 max-w-4xl">
         SESSIONS
         </h2>
